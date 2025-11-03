73340e87b0e5ba32df90a4492fef0fdbf0e48ddb
Archie Meng <archiemeng@protonmail.com>
2025-11-03 18:13:58 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -74,3 +74,4 @@ $(call inherit-product, device/oneplus/sm8750-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/dodge/dodge-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
